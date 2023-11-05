91d9e2458f480c9c46565cabe16bdd505f02bd1a
pauletteObregon <paulette.obregona@gmail.com>
2023-11-05 03:43:29 -0500

    Parte Inicio agregada

--- app/src/main/java/com/example/actividad1prog/MainActivity.kt
+++ app/src/main/java/com/example/actividad1prog/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.actividad1prog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnAEntrenar = findViewById<Button>(R.id.btnAEntrenar)
+
+
+        btnAEntrenar.setOnClickListener {
+            Log.i("MENU -----", "ingreso a menu")
+            navegarHaciaApp(menuIntermedio::class.java) //
+        }
+
+    }
+    fun navegarHaciaApp(clase:Class<*>){
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
