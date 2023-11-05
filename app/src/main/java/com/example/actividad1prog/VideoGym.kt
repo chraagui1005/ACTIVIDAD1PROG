cdc721e85cdda6881b81b605f3fe33ffc1d120ed
Christian Aguilar <christian.aguilarcoronel@alumnos.viu.es>
2023-11-05 00:53:06 -0500

    Se agrega videos de youtube

--- app/src/main/java/com/example/actividad1prog/VideoGym.kt
+++ app/src/main/java/com/example/actividad1prog/VideoGym.kt
@@ -3,6 +3,8 @@ package com.example.actividad1prog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.widget.Button
 
 class VideoGym : AppCompatActivity() {
@@ -10,22 +12,45 @@ class VideoGym : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.videogym)
 
+        val webView: WebView = findViewById(R.id.webView)
+        val video = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/aoHGavS1XPY?si=yoMwJ8-BohlPgeBM\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>"
+        webView.loadData(video, "text/html", "utf-8")
+        webView.settings.javaScriptEnabled = true
+        webView.webChromeClient = WebChromeClient()
+
+
         //PRIMERO RECUPERAR EL BOTTON
         val btnVolver=findViewById<Button>(R.id.btnVolver)
         btnVolver.setOnClickListener {
             Log.i("ACTIVIDAD1PROG", "------BOTON ${btnVolver.id} PULSADO----- boton")
+
         }
         val btnVideoPierna=findViewById<Button>(R.id.btnVideoPierna)
         btnVideoPierna.setOnClickListener {
             Log.i("ACTIVIDAD1PROG", "------BOTON ${btnVideoPierna.id} PULSADO----- boton")
+            val webView: WebView = findViewById(R.id.webView)
+            val video = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/aoHGavS1XPY?si=yoMwJ8-BohlPgeBM\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>"
+            webView.loadData(video, "text/html", "utf-8")
+            webView.settings.javaScriptEnabled = true
+            webView.webChromeClient = WebChromeClient()
         }
         val btnVideoBrazo=findViewById<Button>(R.id.btnVideoBrazo)
         btnVideoBrazo.setOnClickListener {
             Log.i("ACTIVIDAD1PROG", "------BOTON ${btnVideoBrazo.id} PULSADO----- boton")
+            val webView: WebView = findViewById(R.id.webView)
+            val video = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/iIdReY7T-4s?si=AaL0U0cE1pcue-cy\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>"
+            webView.loadData(video, "text/html", "utf-8")
+            webView.settings.javaScriptEnabled = true
+            webView.webChromeClient = WebChromeClient()
         }
         val btnVideoPecho=findViewById<Button>(R.id.btnVideoPecho)
         btnVideoPecho.setOnClickListener {
             Log.i("ACTIVIDAD1PROG", "------BOTON ${btnVideoPecho.id} PULSADO----- boton")
+            val webView: WebView = findViewById(R.id.webView)
+            val video = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/VB09kLgJDo0?si=16ikr0y-yDJrTIOS\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>"
+            webView.loadData(video, "text/html", "utf-8")
+            webView.settings.javaScriptEnabled = true
+            webView.webChromeClient = WebChromeClient()
         }
     }
 }
